083cd132a1cf935933219443c5e40d10ad15332b
Noon <noon@Noons-MacBook-Pro.local>
2018-06-14 16:15:11 -0400

    change port no

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(function(req, res, next){
 
 app.use(require('./routes/index.js'))
 
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 4000);
 app.listen(app.get(port), () => {
     console.log('Listen on Port 3000');
 })
